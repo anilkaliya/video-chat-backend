67fd8300105cbedde40aeb0a8cf71c7a0851e6c3
Shilpee Sinha <shilpeesinha@Shilpees-MBP.domain.name>
2023-07-19 02:58:55 +0530

    added socket handling

--- src/server.ts
+++ src/server.ts
@@ -12,47 +12,47 @@ const io: Server = new Server(server,{cors: {
   origin: '*',
 }
 });
-
-const activeRooms: Map<string, Set<Socket>> = new Map();
+const groupToSocket=new Map<string,string>()
+const socketToGroupId=new Map<string,string>()
 app.use(cors({origin: '*'}));
 app.use(express.json());
 
 app.use('/api', groupRoutes);
+io.on('connection', (socket) => {
+  console.log('A user connected.',socket.id);
 
-io.on('connection', (socket: Socket) => {
-  console.log('A user connected.');
-
+  // Handle 'join' event when a new user joins a room
+  socket.on('room:join', (data) => {
+    const {roomId}=data
+    socket.join(roomId);
+    groupToSocket.set(roomId,socket.id)
+    socketToGroupId.set(socket.id,roomId)
+    io.to(roomId).emit("user:joined",{roomId,socketId:socket.id})
+    socket.join(roomId)
+    io.to(socket.id).emit("room:join",data)
+    
+    console.log(`User joined room: ${roomId}`);
+  });
+  socket.on("user:call",(data:any)=>{
+    const {offer,to}=data
+    io.to(to).emit("incoming:call",{from:socket.id,offer})
+  })
+   
+  socket.on("call:accepted",(data:any)=>{
+    console.log("call accepted")
+    const {to,answer}=data
+    io.to(to).emit("call:accepted",{from:socket.id,answer})
+  })
+  socket.on("peer:nego:needed",(data:any)=>{
+    const {offer,to}=data
+    io.to(to).emit("peer:nego:needed",{from:socket.id,offer})
+  })
+ socket.on("peer:nego:done",(data:any)=>{
+  const {to,answer}=data
+  io.to(to).emit("peer:nego:final",{to:socket.id,answer})
+ })
   socket.on('disconnect', () => {
     console.log('A user disconnected.');
-
-    activeRooms.forEach((sockets: Set<Socket>) => {
-      sockets.delete(socket);
-    });
-  });
-
-  socket.on('join', (roomName: string) => {
-    console.log(`User joined room ${roomName}`);
-
-    if (!activeRooms.has(roomName)) {
-      activeRooms.set(roomName, new Set());
-    }
-
-    const room: Set<Socket> | undefined = activeRooms.get(roomName);
-    room?.add(socket);
-
-    room?.forEach((socketInRoom: Socket) => {
-      if (socketInRoom !== socket) {
-        socketInRoom.emit('userJoined');
-      }
-    });
-
-    socket.on('stream', (stream: MediaStream) => {
-      room?.forEach((socketInRoom: Socket) => {
-        if (socketInRoom !== socket) {
-          socketInRoom.emit('stream', stream);
-        }
-      });
-    });
   });
 });
 
