94b72a9c9dcb0f1f746b5a3c3e5181a71807f087
Shilpee Sinha <shilpeesinha@Shilpees-MacBook-Pro.local>
2023-07-19 17:54:20 +0530

    added socket handling

--- src/server.ts
+++ src/server.ts
@@ -9,11 +9,7 @@ import groupRoutes from './routes/groupRoutes';
 const app: Application = express();
 const server: http.Server = http.createServer(app);
 const io: Server = new Server(server,{cors: {origin:"*"}});
-app.use(cors({
-  origin: 'https://d1g4q894ebfo9f.cloudfront.net',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Authorization'],
-}));
+app.use(cors());
 const groupToSocket=new Map<string,string>()
 const socketToGroupId=new Map<string,string>()
 
